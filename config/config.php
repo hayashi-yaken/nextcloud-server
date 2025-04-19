7d39835968b9b55990ee30d029faf5ec773c275f
hayashi-yaken <mypodo@icloud.com>
2025-04-19 23:55:21 +0900

    feat: configファイルを追加

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$CONFIG = array (
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    1 => 'your.ts.net',
+  ),
+  'overwrite.cli.url' => 'https://your.ts.net',
+  'overwriteprotocol' => 'https',
+);
